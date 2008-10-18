23abe783d3d048c6379d8c571b340f0544d14c7e
Rein Henrichs <reinh@reinh.com>
2008-10-18 15:57:22 -0400

    Update layout, application controller, fix player bug
    - Sign up header area updated with create layout
    - signup header area checks logged in status
    - fix association in player model
    - include auth helper in application controller
    - no layout for xhr requests

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -1,6 +1,6 @@
 class Ladder < ActiveRecord::Base
   has_many   :players, :through => :rank
-  belongs_to :owner, :class_name => :player
+  belongs_to :owner, :class_name => 'Player'
   
   validates_length_of :name, :in => 3..50
   validates_length_of :game, :in => 3..50
