e3d0046f99e578cda35c5cbcd90d26cf8f9c76c0
Rein Henrichs <reinh@reinh.com>
2008-10-18 15:16:18 -0400

    Adding login and signup routes, links in auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
 
   map.resources :ladders
+
+  map.login 'login', :controller => 'sessions', :action => 'new'
+  map.signup 'signup', :controller => 'players', :action => 'new'
 end
