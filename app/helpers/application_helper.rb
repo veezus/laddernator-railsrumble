cbd8457beb2a2ed275a5d0dbaeb3122360f9923b
Zach Inglis <zach@lt3media.com>
2008-10-18 21:45:44 +0100

    Styled up tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def page_title
+    @page_title ? "Ladder Challenge &raquo; #{@page_title}" : "Ladder Challenge"
+  end
 end
