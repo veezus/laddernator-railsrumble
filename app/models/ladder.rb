3ec6f1e3a93238feb6bc033c9d7f0679ac4944ea
Veez (Matt Remsik) <mremsik@gmail.com>
2008-10-19 12:45:35 -0400

    Update validations on Ladder

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -4,6 +4,7 @@ class Ladder < ActiveRecord::Base
   has_many   :challenges
   belongs_to :owner, :class_name => 'Player'
   
+  validates_presence_of :name, :game
   validates_length_of :name, :in => 3..50
   validates_length_of :game, :in => 3..50
   
