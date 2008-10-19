44ffbd54a8f24e15a4012da0d5614466b40617d4
Veez (Matt Remsik) <mremsik@gmail.com>
2008-10-18 21:15:30 -0400

    WIP advancing challenge flow

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,10 +1,16 @@
 class ChallengesController < ApplicationController
-  before_filter :login_required
   resources_controller_for :challenges
+
+  before_filter :login_required
 
   response_for :create do |format|
     format.html do
-      redirect_to :back
+      if @challenge.new_record?
+        flash[:error] = "Couldn't create your challenge!"
+      else
+        flash[:notice] = "Successfully created your challenge!"
+      end
+      redirect_to enclosing_resource
     end
   end
 
@@ -13,6 +19,8 @@ class ChallengesController < ApplicationController
   def new_resource
     returning Challenge.new(params[:challenge]) do |challenge|
       challenge.challenger = current_player
+      challenge.challengee = Player.find(params[:challengee_id])
+      challenge.ladder = enclosing_resource
     end
   end
 end
