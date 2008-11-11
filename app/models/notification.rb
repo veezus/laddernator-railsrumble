69fba74e843e139ed7b6f4a3a77ea69ab268a5ae
Veez (Matt Remsik) <mremsik@gmail.com>
2008-11-11 12:15:17 -0500

    Fix challenged email method

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,8 +2,9 @@ class Notification < ActionMailer::Base
   ActionMailer::Base.default_url_options[:host] ||= "laddernator.com"
 
   def challenged(challenger, challengee)
-    from    "noreply@#{ActionMailer::Base.default_url_options[:host]}"
-    subject "You have been challenged by #{challenger.login}!  You have until the end of the day to respond."
+    from       "noreply@#{ActionMailer::Base.default_url_options[:host]}"
+    recipients challengee.email
+    subject    "You have been challenged by #{challenger.login}!  You have until the end of the day to respond."
   end
 
 end
