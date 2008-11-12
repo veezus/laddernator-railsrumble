bdd6db890fe5e57b20ab44fad3e9f66826049985
Veez (Matt Remsik) <mremsik@gmail.com>
2008-11-12 09:42:47 -0500

    Rejection specs

--- spec/models/ladder_spec.rb
+++ spec/models/ladder_spec.rb
@@ -15,4 +15,25 @@ describe Ladder do
     @ladder.valid?
     @ladder.errors.should be_invalid(:game)
   end
+  describe "#rejections_left_for" do
+    before do
+      @ladder = Ladder.generate!
+      @veez = Player.generate!
+      @durran = Player.generate!
+      @reinh = Player.generate!
+      [@veez, @durran, @reinh].each { |player| @ladder.ranks.create!(:player => player) }
+    end
+    it "should return 3 with no existing challenges" do
+      @ladder.rejections_left_for(@veez).should == 3
+    end
+    it "should return 3 with existing challenges from other players" do
+      2.times { Challenge.create!(:challengee => @durran, :challenger => @reinh, :ladder => @ladder, :status => 'rejected') }
+      @ladder.rejections_left_for(@veez).should == 3
+    end
+    it "should return the correct number of remaining rejections" do
+      2.times { Challenge.create!(:challengee => @durran, :challenger => @reinh, :ladder => @ladder, :status => 'rejected') }
+      2.times { Challenge.create!(:challenger => @durran, :challengee => @veez, :ladder => @ladder, :status => 'rejected') }
+      @ladder.rejections_left_for(@durran).should == 1
+    end
+  end
 end
